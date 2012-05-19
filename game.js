a23bc499ed1c38d5409146b232b951666a4b15eb
Jim Munro <jamescmunro84@gmail.com>
2012-05-18 22:26:43 -0400

    working on proximity detection

---
+++ game.js
@@ -0,0 +1,124 @@
+/** @constructor */
+var Card = function (suit, number){
+    /** @returns {Number} The number of the card in the deck. (1-52) */
+    this.getNumber = function (){
+        return number;  
+    };
+    /** @returns {String} The name of the suit. "Hearts","Clubs","Spades", or "Diamonds." */
+    this.getSuit = function (){
+        var suitName = '';
+        switch (suit){
+            case 1:
+                suitName = "Hearts";
+                break;
+            case 2:
+                suitName = "Clubs";
+                break; 
+            case 3:
+                suitName = "Spades";
+                break; 
+            case 4:
+                suitName = "Diamonds";
+                break;                
+        }
+        return suitName;
+    };
+    /** @returns {String} The HTML-encoded symbol of the suit. */
+    this.getSymbol = function (){
+        var suitName = '';
+        switch (suit){
+            case 1:
+                suitName = "&hearts;";
+                break;
+            case 2:
+                suitName = "&clubs;";
+                break; 
+            case 3:
+                suitName = "&spades;";
+                break; 
+            case 4:
+                suitName = "&diams;";
+                break;                
+        }
+        return suitName;
+    };
+    /** @returns {Number} The value of the card for scoring. */
+    this.getValue = function (){
+        var value = number;
+        if (number >= 10){
+            value = 10;
+        }
+        if(number === 1) {
+            value = 11;
+        }
+        return value;
+    };
+    /** @returns {String} The full name of the card. "Ace of Spades" */
+    this.getName = function (){
+        var cardName = '';
+        switch (number){
+            case 1:
+                cardName = "A";
+                break;
+            case 13:
+                cardName = "K";
+                break;
+            case 12:
+               cardName = "Q";
+                break;
+            case 11:
+                cardName = "J";
+                break;
+            default:
+                cardName = number;
+                break;
+        }
+        return cardName+this.getSymbol();
+    };
+};
+/** @constructor */
+var Deck = function (){
+    var cards = [];
+    /** Creates a new set of cards. */
+    var newCards = function (){
+        var i,
+            suit,
+            number;
+        for (i=0;i<52;i++){
+            suit = i%4+1;
+            number = i%13+1;
+            cards.push(new Card(suit,number));
+        }
+    };
+    /* Create those new cards. */
+    newCards();
+    /** Shuffles the cards. Modifies the private instance of the cards array.
+     * @returns {Array} An array of Cards representing the shuffled version of the deck.
+     */
+    this.shuffle = function (){
+        for(var j, x, i = cards.length; i; j = parseInt(Math.random() * i), x = cards[--i], cards[i] = cards[j], cards[j] = x);
+        return this.getCards();
+    };
+    /** @returns {Array} An array of cards representing the Deck. */
+    this.getCards = function (){
+        return cards;
+    };
+    /** @returns {Card} Deals the top card off the deck. Removes it from the Deck. */
+    this.deal = function (){
+    	this.shuffle();
+        if (!cards.length){
+            console.log("Ran out of cards, new deck");
+            newCards();
+            this.shuffle();
+        }
+        for(var i = 0; i <31; i++){
+        	return cards.pop();
+        }
+        for(var i = 0; i < this.cards.length; i++){
+        	myHole.receiveCard(cards.pop());
+        }
+    };
+    this.hit = function(){
+    	return cards.pop();
+    }
+};
---
+++ tripeaks.js
@@ -0,0 +1,143 @@
+var Field = function(deck){
+	var cards = [],
+		i;
+	for(i = 0; i < 30; i++){
+		cards.push(deck.deal());
+	};
+	this.logCards = function(){
+		console.log("Field cards: " + cards.length);
+	};
+	this.receiveCard = function(card){
+		cards.push(card);
+	};
+	this.toHtml = function(){
+		var arrayOut = [],
+			i;
+		for(i = 0; i < cards.length; i++){
+			arrayOut.push('<div id="card-',  i + 1, '" class="card ',cards[i].getSuit(),' ',cards[i].getNumber(),'">',cards[i].getName(),'</div>');
+		}
+		return arrayOut.join('');
+	};
+	this.removeCard = function(index){
+		var card = cards[index];
+		cards.splice(index, 1);
+		return card;
+	}
+};
+
+var Hole = function(deck){
+	var cards = [];
+	for(i = 0; i < 22; i++){
+		cards.push(deck.deal());
+	}
+	this.logCards = function(){
+		console.log("Hole cards: " + cards.length);
+	}
+	this.hitHand = function(){
+		return cards.pop();
+	};
+	this.receiveCard = function(card){
+		cards.push(card);
+	};
+	this.giveCard = function(){
+		return cards.pop();
+	};
+	this.checkForCards = function(){
+		return cards.length;
+	}
+	this.toHtml = function(){
+		var arrayOut = [],
+			i;
+		for(i = 0; i < cards.length; i++){
+			arrayOut.push('<div class="card ',cards[i].getSuit(),' ',cards[i].getNumber(),'">',cards[i].getName(),'</div>');
+		}
+		return arrayOut.join('');
+	};
+};
+
+var Hand = function(deck){
+	var cards = [];
+	
+	this.receiveCard = function(card){
+		cards.push(card);
+	};
+	this.logCards = function(){
+		console.log("Hand cards: " + cards.length);
+	};
+	this.toHtml = function(){
+		var arrayOut = [],
+			i,
+			topCard = cards.length - 1;
+		
+		arrayOut.push('<div class="card handCard ',cards[topCard].getSuit(),' ',cards[topCard].getNumber(),'">',cards[topCard].getName(),'</div>');
+		return arrayOut.join('');
+	};
+	this.getTopCard = function(){
+		return cards[(cards.length - 1)];
+	}
+};
+
+(function(){
+	var myDeck = new Deck();
+	var myHole = new Hole(myDeck);
+	var myHand = new Hand(myDeck);
+	var myField = new Field(myDeck);
+	
+	var $field = $("#field");
+	var $hole = $("#hole");
+	var $hand = $("#hand");
+	var $card = $(".card");
+	
+	myDeck.shuffle();
+	myDeck.deal();
+	$field.html(myField.toHtml());
+	var logCards = function(){
+		myField.logCards();
+		myHole.logCards();
+		myHand.logCards();	
+	}
+	
+	$hole.on("click", function(){
+		myHand.receiveCard(myHole.hitHand());
+		
+		$hand.html(myHand.toHtml());
+		logCards();
+		if(!!myHole.checkForCards()){
+			return;	
+		} else{
+			$hole.hide();
+		}
+	});
+	
+	$(".card").on("click", function(){
+		var $clicked = $(this);
+		var clickedTop = parseInt($clicked.css("top"));
+		var clickedLeft = parseInt($clicked.css("left"));
+		var locked = false;
+		var clickedIndex = ($clicked.attr("id").split("-")[1]) - 1;
+		console.log("index: " + clickedIndex);
+		$(".card").each(function(){
+			var $this = $(this);
+			var thisTop = parseInt($this.css("top"));
+			var thisLeft = parseInt($this.css("left"));
+			
+			if(((thisTop < (clickedTop + 50)) && (thisTop > clickedTop)) && (((thisLeft > (clickedLeft - 20)) && (thisLeft < clickedLeft)) || ((thisRight > (clickedRight - 20)) && (thisRight < clickedRight)))){
+				locked = true;
+				
+			}
+			//console.log("top: " +parseInt($(this).css("top")));
+			//console.log("left: " +parseInt($(this).css("left")));
+			
+		});
+		if(!locked){
+			//console.log("this card is free");
+			myHand.receiveCard(myField.removeCard(clickedIndex));
+			$clicked.hide();
+			$hand.html(myHand.toHtml());
+		} else{
+			console.log("this card is locked");
+		}
+			
+	});
+	
+}());
