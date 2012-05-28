5ba7ceafe50a4e99571fb5d54eaa0852a19d3813
Scottux <d.scott.carroll@gmail.com>
2012-05-27 20:40:59 -0400

    adding card images

--- tripeaks.js
+++ tripeaks.js
@@ -1,124 +1,129 @@
-var Field = function(deck){
+var Field = function (deck){
 	var cards = [],
 		i;
-	this.init = function(){
+	this.init = function (){
 		cards = [];
+		var i;
 		for(i = 0; i < 30; i++){
 			cards.push(deck.deal());
-		};
+		}
 	};
 
-	this.logCards = function(){
+	this.logCards = function (){
 		console.log("Field cards: " + cards.length);
 	};
-	this.receiveCard = function(card){
+	this.receiveCard = function (card){
 		cards.push(card);
 	};
-	this.returnCard = function(index){
+	this.returnCard = function (index){
 		return cards[index];
 	};
-	this.toHtml = function(){
+	this.toHtml = function (){
 		var arrayOut = [],
 			i;
 		for(i = 0; i < cards.length; i++){
-			arrayOut.push('<div id="card-',  i + 1, '" class="card fieldCard ',cards[i].getSuit(),' ',cards[i].getNumber(),'">',cards[i].getName(),'</div>');
+			//arrayOut.push('<div id="card-',  i + 1, '" class="card fieldCard ',cards[i].getSuit(),' ',cards[i].getNumber(),'">',cards[i].getName(),'</div>');
+			arrayOut.push('<img id="card-',  i + 1, '" class="card fieldCard" src="images/cards/default/',String(cards[i].getName()).toLowerCase(),'_of_',cards[i].getSuit().toLowerCase(),'.png" alt="',cards[i].getFullName(),'" />');
 		}
 		return arrayOut.join('');
 	};
-	this.removeCard = function(index){
+	this.removeCard = function (index){
 		return cards[index];
 	};
 	this.init();
 };
 
-var Hole = function(deck){
+var Hole = function (deck){
 	var cards = [];
 
-	this.init = function(){
+	this.init = function (){
 		cards = [];
+		var i;
 		for(i = 0; i < 22; i++){
 			cards.push(deck.deal());
-		};
+		}
 	};
-	this.logCards = function(){
+	this.logCards = function (){
 		console.log("Hole cards: " + cards.length);
-	}
-	this.hitHand = function(){
+	};
+	this.hitHand = function (){
 		return cards.pop();
 	};
-	this.receiveCard = function(card){
+	this.receiveCard = function (card){
 		cards.push(card);
 	};
-	this.giveCard = function(){
+	this.giveCard = function (){
 		return this.hitHand();
 	};
-	this.checkForCards = function(){
+	this.checkForCards = function (){
 		return cards.length;
-	}
-	this.toHtml = function(){
+	};
+	this.toHtml = function (){
 		var arrayOut = [],
 			i;
 		for(i = 0; i < cards.length; i++){
-			arrayOut.push('<div class="card ',cards[i].getSuit(),' ',cards[i].getNumber(),'">',cards[i].getName(),'</div>');
+			//arrayOut.push('<div class="card ',cards[i].getSuit(),' ',cards[i].getNumber(),'">',cards[i].getName(),'</div>');
+			arrayOut.push('<img class="card" src="images/cards/default/',String(cards[i].getName()).toLowerCase(),'_of_',cards[i].getSuit().toLowerCase(),'.png" alt="',cards[i].getFullName(),'" />');
 		}
 		return arrayOut.join('');
 	};
 	this.init();
 };
 
-var Hand = function(deck){
+var Hand = function (deck){
 	var cards = [];
 
-	this.receiveCard = function(card){
+	this.receiveCard = function (card){
 		cards.push(card);
 	};
-	this.logCards = function(){
+	this.logCards = function (){
 		console.log("Hand cards: " + cards.length);
 	};
-	this.toHtml = function(){
+	this.toHtml = function (){
 		var arrayOut = [],
 			i,
 			topCard = cards.length - 1;
 
-		arrayOut.push('<div class="card handCard ',cards[topCard].getSuit(),' ',cards[topCard].getNumber(),'">',cards[topCard].getName(),'</div>');
+		//arrayOut.push('<div class="card handCard ',cards[topCard].getSuit(),' ',cards[topCard].getNumber(),'">',cards[topCard].getName(),'</div>');
+		arrayOut.push('<img class="card handCard" src="images/cards/default/',String(cards[topCard].getName()).toLowerCase(),'_of_',cards[topCard].getSuit().toLowerCase(),'.png" alt="',cards[topCard].getFullName(),'" />');
 		return arrayOut.join('');
 	};
-	this.getTopCard = function(){
+	this.getTopCard = function (){
 		return cards[(cards.length - 1)];
-	}
-	this.getValue = function(){
+	};
+	this.getValue = function (){
 		var topCard = this.getTopCard();
 		return topCard.getNumber();
-	}
-	this.empty = function(){
+	};
+	this.empty = function (){
 		cards = [];
-	}
+	};
 };
 
-var Score = function(){
+var Score = function (){
 	var value = 0;
 	var incrementer = 1;
 
-	this.addToScore = function(){
+	this.addToScore = function (){
 		value = value + incrementer;
 		incrementer ++;
 	};
-	this.removeFromScore = function(){
+	this.removeFromScore = function (){
 		console.log("removing from score");
 		value = value - 3;
 		incrementer = 1;
 	};
 
-	this.getScore = function(){
+	this.getScore = function (){
 		return value;
 	};
 };
 
-(function(){
-	var myDeck = new Deck();
-	var myHole = new Hole(myDeck);
-	var myHand = new Hand(myDeck);
-	var myField = new Field(myDeck);
+(function (){
+	var myDeck;
+	var myHole;
+	var myHand;
+	var myField;
 	var myScore = new Score();
 
 	var $field = $("#field");
@@ -129,19 +134,28 @@ var Score = function(){
 	var $cardsLeft = $("#cardsLeft");
 	var $newHand = $("#newHand");
 
-	myDeck.shuffle();
-	myDeck.deal();
-	$field.html(myField.toHtml());
-	myHand.receiveCard(myHole.hitHand());
-	$hand.html(myHand.toHtml());
-	$cardsLeft.html(myHole.checkForCards());
-	var logCards = function(){
+	var init = function (){
+		myDeck = new Deck();
+		myHole = new Hole(myDeck);
+		myHand = new Hand(myDeck);
+		myField = new Field(myDeck);
+		myDeck.shuffle();
+		myDeck.deal();
+		$field.html(myField.toHtml());
+		myHand.empty();
+		myHand.receiveCard(myHole.hitHand());
+		$hand.html(myHand.toHtml());
+		$cardsLeft.html(myHole.checkForCards());
+		console.log("You wish to have a new hand.");
+	};
+	
+	var logCards = function (){
 		myField.logCards();
 		myHole.logCards();
 		myHand.logCards();
 	};
 
-	$hole.on("click", function(){
+	$hole.on("click", function (){
 		myHand.receiveCard(myHole.hitHand());
 
 		$hand.html(myHand.toHtml());
@@ -156,7 +170,7 @@ var Score = function(){
 
 	});
 
-	$(".fieldCard").on("click", function(){
+	$(document).on("click", ".fieldCard", function (){
 		var $clicked = $(this);
 		var clickedTop = parseInt($clicked.css("top"));
 		var clickedLeft = parseInt($clicked.css("left"));
@@ -170,7 +184,7 @@ var Score = function(){
 		console.log("index: " + clickedIndex);
 		console.log("value: " + clickedValue);
 		console.log("text: " + $clicked.attr("class"));
-		$(".fieldCard").each(function(){
+		$(".fieldCard").each(function (){
 			var $this = $(this);
 			var thisTop = parseInt($this.css("top"));
 			var thisLeft = parseInt($this.css("left"));
@@ -218,19 +232,8 @@ var Score = function(){
 
 	});
 
-	$newHand.on("click", function(){
-		myDeck = new Deck();
-		myHole = new Hole(myDeck);
-		myHand = new Hand(myDeck);
-		myField = new Field(myDeck);
-		myDeck.shuffle();
-		myDeck.deal();
-		$field.html(myField.toHtml());
-		myHand.empty();
-		myHand.receiveCard(myHole.hitHand());
-		$hand.html(myHand.toHtml());
-		$cardsLeft.html(myHole.checkForCards());
-		console.log("You wish to have a new hand.");
+	$newHand.on("click", function (){
+		init();
 	});
-
+	init();
 }());
